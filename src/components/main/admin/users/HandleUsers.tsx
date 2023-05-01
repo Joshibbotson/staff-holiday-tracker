42afef162580c7d9a1790c385084b7168b0b96dc
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2023-05-01 12:01:59 +0100

    BUILD - functional user tab, requires further polish and user profile needs to be updateable, also ensure user can't select or choose self

--- src/components/main/admin/users/HandleUsers.tsx
+++ src/components/main/admin/users/HandleUsers.tsx
@@ -3,11 +3,17 @@ import SCSS from "./handleusers.module.scss";
 import { useContext, useEffect, useState } from "react";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import SearchIcon from "@mui/icons-material/Search";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { UserType } from "../../../../types";
+import { sl } from "date-fns/locale";
 
 export const HandleUsers = () => {
     const { users } = useContext(UsersContext);
     const [searchValue, setSearchValue] = useState<string>("");
-    const [filteredUsers, setFilteredUsers] = useState(users);
+    const [filteredUsers, setFilteredUsers] = useState<UserType[]>(users);
+    const [selectedUser, setSelectedUser] = useState<UserType | undefined>(
+        undefined
+    );
 
     useEffect(() => {
         setFilteredUsers(
@@ -38,8 +44,15 @@ export const HandleUsers = () => {
                     {filteredUsers.map(user => {
                         return (
                             <div
-                                className={SCSS.mainGrid__userTab}
+                                className={
+                                    user === selectedUser
+                                        ? SCSS.mainGrid__selectedUserTab
+                                        : SCSS.mainGrid__userTab
+                                }
                                 key={user.uid}
+                                onClick={() => {
+                                    setSelectedUser(user);
+                                }}
                             >
                                 <div className={SCSS.userTab__profilePic}>
                                     <AccountCircleIcon fontSize="inherit" />
@@ -48,11 +61,56 @@ export const HandleUsers = () => {
                                     <h4>{user.name}</h4>
                                     <p>{user.email}</p>
                                 </div>
+                                <ChevronRightIcon
+                                    className={SCSS.userTab__chevronRight}
+                                />
                             </div>
                         );
                     })}
                 </div>
-                <div className={SCSS.mainGrid__userProfile}></div>
+                <div className={SCSS.mainGrid__userProfile}>
+                    <div className={SCSS.userProfile__topContainer}>
+                        <div className={SCSS.topContainer__profilePic}>
+                            <AccountCircleIcon fontSize="inherit" />
+                        </div>
+                        <div className={SCSS.topContainer__name}>
+                            {selectedUser ? selectedUser.name : "name"}
+                        </div>
+                        <div className={SCSS.topContainer__email}>
+                            {selectedUser
+                                ? selectedUser.email
+                                : "email@hotmail.com"}
+                        </div>
+                    </div>
+                    <div className={SCSS.userProfile__statsContainer}>
+                        <div className={SCSS.statsContainer__holidaytime}>
+                            <h1>Holiday and absence:</h1>
+                            <p>
+                                Remaining Holidays:{" "}
+                                {selectedUser
+                                    ? selectedUser.remainingHolidays
+                                    : 0}
+                            </p>
+                            <p>
+                                Taken Holidays:{" "}
+                                {selectedUser ? selectedUser.takenHolidays : 0}
+                            </p>
+                            <p>
+                                Flextime:{" "}
+                                {selectedUser ? selectedUser.flexTime : 0}
+                            </p>
+                        </div>
+                        <div className={SCSS.statsContainer__holidaytime}>
+                            <h1>Additional Info:</h1>
+                            <p>
+                                National Holidays:{" "}
+                                {selectedUser
+                                    ? selectedUser.nationalHolidays
+                                    : "-"}
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
