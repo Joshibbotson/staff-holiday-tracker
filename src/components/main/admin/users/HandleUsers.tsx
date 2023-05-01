13cf3b6cbbc7e2756da64fa93a15336263aae149
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2023-05-01 10:36:44 +0100

    BUILD - users page now has search bar capabilty

--- src/components/main/admin/users/HandleUsers.tsx
+++ src/components/main/admin/users/HandleUsers.tsx
@@ -1,18 +1,46 @@
 import { UsersContext } from "../../../../context/UsersContext";
 import SCSS from "./handleusers.module.scss";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import SearchIcon from "@mui/icons-material/Search";
 
 export const HandleUsers = () => {
     const { users } = useContext(UsersContext);
+    const [searchValue, setSearchValue] = useState<string>("");
+    const [filteredUsers, setFilteredUsers] = useState(users);
+
+    useEffect(() => {
+        setFilteredUsers(
+            users.filter(user => {
+                return user.name.toLowerCase().includes(searchValue);
+            })
+        );
+    }, [searchValue]);
+
     return (
         <>
-            <div className={SCSS.header}></div>
+            <div className={SCSS.header}>
+                <div className={SCSS.header__searchContainer}>
+                    <SearchIcon className={SCSS.header__magnifiyingGlass} />
+                    <input
+                        type="text"
+                        name="search"
+                        value={searchValue}
+                        onChange={e => {
+                            return setSearchValue(e.target.value.toLowerCase());
+                        }}
+                    />
+                </div>
+                <h1>Users</h1>
+            </div>
             <div className={SCSS.mainGrid}>
                 <div className={SCSS.mainGrid__userList}>
-                    {users.map(user => {
+                    {filteredUsers.map(user => {
                         return (
-                            <div className={SCSS.mainGrid__userTab}>
+                            <div
+                                className={SCSS.mainGrid__userTab}
+                                key={user.uid}
+                            >
                                 <div className={SCSS.userTab__profilePic}>
                                     <AccountCircleIcon fontSize="inherit" />
                                 </div>
