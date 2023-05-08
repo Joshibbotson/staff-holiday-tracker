1f1f1fa91744ca9f9477b541185483a0c2beba7b
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2023-05-08 19:18:56 +0100

    BUILD - created random colour generator for user tab colours

---
+++ src/util-functions/randomColour.ts
@@ -0,0 +1,7 @@
+function randomColour() {
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+
+    return `rgb(${red}, ${green}, ${blue})`;
+}
