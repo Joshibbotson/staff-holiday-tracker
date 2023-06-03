ec8caa6a77c63573aa4df5e5f7af356dd5173d2c
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2023-06-03 23:05:12 +0100

    STYLE - styling update for 404 page

--- src/pages/404-page/Custom404Page.tsx
+++ src/pages/404-page/Custom404Page.tsx
@@ -1,15 +1,18 @@
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import SCSS from "./custom404Page.module.scss";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase/auth/auth";
 export default function Custom404Page() {
     const navigate = useNavigate();
 
     function navigateToRoute() {
         navigate(`/`);
     }
+    const [user] = useAuthState(auth);
 
     return (
-        <article className={SCSS.container404}>
+        <article className={user ? SCSS.container404 : SCSS.container404NoUser}>
             <div className={SCSS.container404__graphic}></div>
             <div className={SCSS.container404__module}>
                 <h1>404 Page Not Found</h1>
