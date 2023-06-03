eb5f4808fdfcd6d159e9e69c655b135771838c5b
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2023-06-03 20:44:53 +0100

    FIX - pathing

--- src/pages/404-page/Custom404Page.tsx
+++ src/pages/404-page/Custom404Page.tsx
@@ -1,43 +1,17 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Custom404Page() {
-    const [validUrl, setValidUrl] = useState<boolean>(false);
     const navigate = useNavigate();
-
-    checkUrlIsValid(window.location.pathname);
-
-    function checkUrlIsValid(url: string) {
-        const validUrls = [
-            "/",
-            "/myrequests",
-            "/handlerequests",
-            "/users",
-            "/verifyemailsent",
-            "/login",
-            "/reset",
-            "/register",
-        ];
-
-        validUrls.includes(url) ? setValidUrl(true) : setValidUrl(false);
-        return validUrls.includes(url) ? navigate(url) : null;
-    }
 
     function navigateToRoute() {
         navigate(`/`);
     }
 
     return (
-        <>
-            {validUrl ? null : (
-                <div>
-                    <h1>404 Page Not Found</h1>
-                    <p>Oops! The page you requested does not exist.</p>
-                    <button onClick={navigateToRoute}>
-                        Go back to My Requests
-                    </button>
-                </div>
-            )}
-        </>
+        <div>
+            <h1>404 Page Not Found</h1>
+            <p>Oops! The page you requested does not exist.</p>
+            <button onClick={navigateToRoute}>Go back home</button>
+        </div>
     );
 }
